d675489a4e0302553ef7feb640673cf9ba8f6f17
VietAnh188 <anhkun123456@gmail.com>
2023-03-25 00:37:56 +0700

    refactor

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import heroImage from "../../assets/images/hero-image.png"
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
@@ -14,7 +15,7 @@ const Register = () => {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900">
             Welcome to
           </h2>
-          <h2 className="text-[#6C63FF] text-3xl">
+          <h2 className="text-primary text-3xl">
             Go Backpacking
           </h2>
         </div>
@@ -28,7 +29,7 @@ const Register = () => {
           <div className="-space-y-px rounded-md">
             <div className="lg:flex">
               <div className="lg:w-1/2 lg:pr-4 pb-4">
-                <label for="firstName" className="sr-only">
+                <label htmlFor="firstName" className="sr-only">
                   First Name
                 </label>
                 <input
@@ -36,12 +37,12 @@ const Register = () => {
                   name="firstName"
                   type="text"
                   required
-                  className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-[#F2F2F2] placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
+                  className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-gray-primary placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
                   placeholder="First Name"
                 />
               </div>
               <div className="lg:w-1/2">
-                <label for="lastName" className="sr-only">
+                <label htmlFor="lastName" className="sr-only">
                   Last Name
                 </label>
                 <input
@@ -49,14 +50,14 @@ const Register = () => {
                   name="lastName"
                   type="text"
                   required
-                  className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-[#F2F2F2] placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
+                  className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-gray-primary placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
                   placeholder="Last Name"
                 />
               </div>
             </div>
             <div className="pb-4">
               <label
-                for="email-address"
+                htmlFor="email-address"
                 className="sr-only"
               >
                 Email
@@ -65,15 +66,15 @@ const Register = () => {
                 id="email-address"
                 name="email"
                 type="email"
-                autocomplete="email"
+                autoComplete="email"
                 required
-                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-[#F2F2F2] placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
+                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-gray-primary placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
                 placeholder="Email address"
               />
             </div>
             <div className="pb-4">
               <label
-                for="email-address"
+                htmlFor="email-address"
                 className="sr-only"
               >
                 Password
@@ -83,12 +84,12 @@ const Register = () => {
                 name="password"
                 type="password"
                 required
-                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-[#F2F2F2] placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
+                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-gray-primary placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
                 placeholder="Password"
               />
             </div>
             <div className="pb-4">
-              <label for="password" className="sr-only">
+              <label htmlFor="password" className="sr-only">
                 Confirm Password
               </label>
               <input
@@ -96,7 +97,7 @@ const Register = () => {
                 name="confirmPassword"
                 type="password"
                 required
-                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-[#F2F2F2] placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
+                className="relative block w-full outline-none rounded-md text-black py-3 px-3 bg-gray-primary placeholder:text-black focus:z-10 sm:text-sm sm:leading-6"
                 placeholder="Confirm Password"
               />
             </div>
@@ -105,18 +106,17 @@ const Register = () => {
           <div className="my-10">
             <button
               type="submit"
-              className="group relative flex w-full justify-center rounded-md bg-[#6C63FF] py-4 px-3 text-lg font-medium tracking-wider text-white hover:opacity-90 duration-300"
+              className="group relative flex w-full justify-center rounded-md bg-primary py-4 px-3 text-lg font-medium tracking-wider text-white hover:opacity-90 duration-300"
             >
               Sign up
             </button>
           </div>
           <div className="flex justify-center text-sm">
             <span>
-              Already have an account?
-              <a className="text-[#6C63FF]" href="#">
-                {" "}
+              Already have an account?{' '}
+              <Link to='/login' className="text-primary">
                 Login
-              </a>
+              </Link>
             </span>
           </div>
         </form>
