c2c5ba3946d85beb3570a21a9318771d5c3eb0b3
VietAnh188 <anhkun123456@gmail.com>
2023-03-25 01:00:55 +0700

    register logic

--- src/containers/register/register.container.jsx
+++ src/containers/register/register.container.jsx
@@ -1,8 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { Register } from '../../components'
 
 const RegisterContainer = () => {
-  return <Register />
+  const [registerData, setRegisterData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const handleInputRegisterData = (key, value) => {
+    setRegisterData(state => ({ ...state, [key]: value }))
+  }
+
+  const handleSubmitRegister = () => {
+    const { firstName, lastName, email, password, confirmPassword } = registerData
+    if (firstName && lastName && email && password && confirmPassword) {
+      if (password === confirmPassword) {
+        console.log('register')
+      }
+    } else {
+      console.log('error')
+    }
+  }
+
+  return <Register data={registerData} onInput={handleInputRegisterData} onSubmit={handleSubmitRegister} />
 }
 
 export default RegisterContainer
