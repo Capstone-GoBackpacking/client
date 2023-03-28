f356572fae6420caae98495d261cc004de4d8de8
VietAnh188 <anhkun123456@gmail.com>
2023-03-29 00:03:22 +0700

    active nar tab

--- src/containers/navbar/navbar.container.jsx
+++ src/containers/navbar/navbar.container.jsx
@@ -1,12 +1,21 @@
 import { TabMenu } from 'primereact/tabmenu'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const NavbarContainer = ({ data }) => {
   const navigate = useNavigate()
+  const [active, setActive] = useState(0)
 
   return (
     <div>
-      <TabMenu model={data} onTabChange={(e) => navigate(e.value.path)} />
+      <TabMenu
+        model={data}
+        activeIndex={active}
+        onTabChange={(e) => {
+          navigate(e.value.path)
+          setActive(e.index)
+        }}
+      />
     </div>
   )
 }
