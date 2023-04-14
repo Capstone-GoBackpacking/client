7e279dd6d581e1b1c98b85cd6c91a61480343b02
VietAnh188 <anhkun123456@gmail.com>
2023-04-15 06:46:32 +0700

    rightbar location detail

--- src/pages/location-detail/location-detail.page.jsx
+++ src/pages/location-detail/location-detail.page.jsx
@@ -1,9 +1,10 @@
 import { ImageCarousel } from "src/components";
-import { LocationInfo, Navbar } from "src/containers";
+import { LocationInfo, MyMap, Navbar } from "src/containers";
 import { LocationDetailRoute } from "src/routes/route-name";
 import { AiOutlineSetting, AiFillCamera } from "react-icons/ai";
 import { BsFillChatRightDotsFill } from "react-icons/bs";
 import { Outlet } from "react-router-dom";
+import { Marker } from "react-leaflet";
 
 const LocationDetail = () => {
   const images = [
@@ -52,6 +53,35 @@ const LocationDetail = () => {
     lng: "8",
   };
 
+  const relateTrips = [
+    {
+      id: "1",
+      name: "hahahahahahah",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem aliquid officiis maxime quidem tempore ducimus",
+      host: "Le Viet Anh",
+      joined: 2,
+      limit: 4,
+      from: "this is from",
+      to: "this is to",
+      thumbnail:
+        "https://www.shutterstock.com/image-photo/one-person-breaks-formation-violation-260nw-1940349595.jpg",
+    },
+    {
+      id: "2",
+      name: "hahahahahahah",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem aliquid officiis maxime quidem tempore ducimus",
+      host: "Le Viet Anh",
+      joined: 2,
+      limit: 4,
+      from: "this is from",
+      to: "this is to",
+      thumbnail:
+        "https://www.shutterstock.com/image-photo/one-person-breaks-formation-violation-260nw-1940349595.jpg",
+    },
+  ];
+
   const navItems = [
     {
       label: "Reviews",
@@ -77,7 +107,7 @@ const LocationDetail = () => {
   return (
     <div id="location-detail" className="w-11/12 m-auto">
       <ImageCarousel data={images} />
-      <div className="flex">
+      <div className="flex gap-2">
         <div className="flex-2">
           <LocationInfo
             thumbnail={target.thumbnail}
@@ -93,7 +123,36 @@ const LocationDetail = () => {
             <Outlet />
           </div>
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1">
+          <div className="h-80">
+            <MyMap>
+              <Marker position={{ lat: target.lat, lng: target.lng }}></Marker>
+            </MyMap>
+          </div>
+          <div className="mt-5">
+            <label>Relate trips</label>
+            <div>
+              {relateTrips.map((trip) => (
+                <div key={trip.id} className="mt-4 shadow-md bg-white">
+                  <div className="flex gap-2 p-2">
+                    <img
+                      src={trip.thumbnail}
+                      alt={trip.name}
+                      className="w-28"
+                    />
+                    <div>
+                      <p className="font-bold">{trip.name}</p>
+                      <p>{trip.host}</p>
+                      <span>
+                        {trip.joined} / {trip.limit}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
